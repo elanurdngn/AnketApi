325b6ca8b21845c43e374bed45e45fca646148bb
Elanurdngn <73476047+elanurdngn@users.noreply.github.com>
2022-06-02 10:29:45 +0300

    Add project files.

---
+++ myoBlog29API/ViewModel/CevapModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myoBlog29API.ViewModel
+{
+    public class CevapModel
+    {
+        public int CevapId { get; set; }
+        public Nullable<int> UyeId { get; set; }
+        public Nullable<int> MakaleId { get; set; }
+        public Nullable<int> Cevap1 { get; set; }
+    }
+}
